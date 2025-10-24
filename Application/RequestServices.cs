8cb449c6749b4c0954f4b83cbb6611cc6e286118
Aleksey <lekseysz@yandex.ru>
2025-10-24 20:59:44 +0300

    req service

---
+++ Application/RequestServices.cs
@@ -0,0 +1,19 @@
+using Back.Domain.Entity;
+
+namespace Back.Application;
+
+public class RequestServices
+{
+    public RequestServices()
+    {
+        
+    }
+
+    public Guid CreateRequest(Guid userId, string name, string text)
+    {
+        // TODO: check if user exists
+        var request = new Request(userId,  name, text);
+        // TODO: add req to repo
+        return request.Id;
+    }
+}
