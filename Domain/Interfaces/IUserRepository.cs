a364ef922afc889ff1c6e5bb4f4ee475c86bdeae
HemulenEXE <akaseev3@mail.ru>
2025-10-24 21:08:28 +0300

    fix

--- Domain/Interfaces/IUserRepository.cs
+++ Domain/Interfaces/IUserRepository.cs
@@ -6,7 +6,7 @@ namespace Back.Domain.Interfaces
     {
         public Guid AddUser(User user);
         public User GetUser(Guid id);
-        public User GetAllUser();
+        public IEnumerable<User> GetAllUser();
         public void UpdateUser(User user);
         public void DeleteUser(Guid id);
     }
