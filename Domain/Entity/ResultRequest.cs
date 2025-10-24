f6d508e31f74e96ab3f5aded524194d3edb76d3e
HemulenEXE <akaseev3@mail.ru>
2025-10-24 20:33:21 +0300

    add resreqentity

---
+++ Domain/Entity/ResultRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Back.Domain.Entity
+{
+    public class ResultRequest
+    {
+        public Guid Id { get; set; }
+        public Request Request { get; set; }
+        public User UserRequest { get; set; }
+        public IEnumerable<User> ResultRequestUsers { get; set; }
+        public ResultRequest(Guid id, Request request, User userRequest, IEnumerable<User> resultRequestUsers)
+        {
+            Id = id;
+            Request = request;
+            UserRequest = userRequest;
+            ResultRequestUsers = resultRequestUsers;
+        }
+    }
+}
