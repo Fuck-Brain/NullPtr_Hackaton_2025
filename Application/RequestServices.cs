b18b429096866d61b6a83098a11ff8e246431a23
lekseysz <lekseysz@yandex.ru>
2025-10-24 23:37:06 +0300

    response get/post

--- Application/RequestServices.cs
+++ Application/RequestServices.cs
@@ -1,3 +1,5 @@
+using Back.Application.Dtos;
+using Back.Application.Exceptions;
 using Back.Domain.Entity;
 using Back.Domain.Interfaces;
 
@@ -6,16 +8,28 @@ namespace Back.Application;
 public class RequestServices
 {
     private readonly IRequestRepository _requestRepository;
-    public RequestServices(IRequestRepository repository)
+    private readonly AnalyticsClient _analyticsClient;
+    private readonly IUserRepository _userRepository;
+    private readonly IResultRequestRepository _resultRepository;
+    public RequestServices(IRequestRepository repository, AnalyticsClient analyticsClient, IUserRepository userRepository, IResultRequestRepository resultRequestRepository)
     {
         _requestRepository = repository;
+        _analyticsClient = analyticsClient;
+        _userRepository = userRepository;
+        _resultRepository = resultRequestRepository;
     }
 
-    public Guid CreateRequest(Guid userId, string name, string text)
+    public async Task<Guid> CreateRequest(Guid userId, string name, string text)
     {
+        var user = _userRepository.GetUser(userId);
+        if (user is null)
+            throw new AuthException();
+        
         var request = new Request(userId,  name, text);
         _requestRepository.AddRequest(request);
-        // TODO: send req to ml
+        var response = await _analyticsClient.ClassifyAsync(new ClassificationRequest(request, _userRepository.GetAllUser().ToList()));
+        var requestResult = new ResultRequest(request, user, response.ClassifiedUsers);
+        // TODO: save
         return request.Id;
     }
 
